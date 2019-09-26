250a50e2af23b830cb781d64df0047448d677a66
davidsmithitsandbox <davidsmith@itsandbox.net>
2019-09-26 16:11:33 -0400

    Initial commit.

--- src/router.js
+++ src/router.js
@@ -1,10 +1,14 @@
 import Vue from "vue";
 import Router from "vue-router";
-import Home from "./views/Home.vue";
+import Home from "./views/Home/Index.vue";
+import Users from "./views/Users/Index.vue";
+import UsersShow from "./views/Users/Show.vue";
+import NotFound from "./views/Msc/NotFound";
 
 Vue.use(Router);
 
 export default new Router({
+  mode: "history",
   routes: [
     {
       path: "/",
@@ -12,13 +16,28 @@ export default new Router({
       component: Home
     },
     {
-      path: "/about",
-      name: "about",
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () =>
-        import(/* webpackChunkName: "about" */ "./views/About.vue")
+      path: "/users",
+      name: "users",
+      component: Users
+    },
+    {
+      path: "/users/:username",
+      name: "users-show",
+      component: UsersShow,
+      props: true
+    },
+    {
+      path: "*",
+      component: NotFound
     }
+    // {
+    //   // path: "/about",
+    //   // name: "about",
+    //   // route level code-splitting
+    //   // this generates a separate chunk (about.[hash].js) for this route
+    //   // which is lazy-loaded when the route is visited.
+    //   // component: () =>
+    //   //   import(/* webpackChunkName: "about" */ "./views/About.vue")
+    // }
   ]
 });
